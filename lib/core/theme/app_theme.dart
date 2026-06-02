8d4a141d27f5cca8802572be2b271da7f1420a3c
ojas-mecha <ojasc@ext.mechasystems.com>
2026-06-02 11:44:55 +0530

    fix: add localization, page transition and remove ripple effect

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -20,11 +20,36 @@ class AppTheme {
       fontFamily: 'Sora',
       primaryColor: AppColors.accent,
       scaffoldBackgroundColor: AppColors.background,
-      appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent, surfaceTintColor: Colors.transparent),
+      splashFactory: NoSplash.splashFactory,
+      hoverColor: Colors.transparent,
+      highlightColor: Colors.white.withAlpha(25),
+      splashColor: Colors.white.withAlpha(25),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.transparent,
+        surfaceTintColor: Colors.transparent,
+      ),
       scrollbarTheme: const ScrollbarThemeData(
         radius: Radius.circular(4),
         thickness: WidgetStatePropertyAll(4),
         thumbColor: WidgetStatePropertyAll(AppColors.textGrey),
+      ),
+      pageTransitionsTheme: const PageTransitionsTheme(
+        builders: {TargetPlatform.linux: CupertinoPageTransitionsBuilder()},
+      ),
+      iconButtonTheme: IconButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: const WidgetStatePropertyAll(AppColors.textPrimary),
+          overlayColor: WidgetStateProperty.resolveWith((states) {
+            if (states.contains(WidgetState.pressed)) {
+              return Colors.white.withAlpha(25);
+            }
+            if (states.contains(WidgetState.hovered)) {
+              return Colors.transparent;
+            }
+            return null;
+          }),
+          splashFactory: NoSplash.splashFactory,
+        ),
       ),
       textTheme: const TextTheme(
         displayLarge: TextStyle(
@@ -38,8 +63,8 @@ class AppTheme {
           color: AppColors.textPrimary,
         ),
         displaySmall: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w300,
+          fontSize: 24,
+          fontWeight: FontWeight.w400,
           color: AppColors.textDim,
         ),
         headlineMedium: TextStyle(
--- lib/features/navigation/presentation/screens/main_navigation_container.dart
+++ lib/features/navigation/presentation/screens/main_navigation_container.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:mechanix_clock/features/alarm/presentation/screens/alarm_list_screen.dart';
-import 'package:mechanix_clock/features/stopwatch/presentation/screens/stopwatch_screen.dart';
 import 'package:mechanix_clock/features/alarm/presentation/widgets/bottom_nav_bar.dart';
+import 'package:mechanix_clock/features/stopwatch/presentation/screens/stopwatch_screen.dart';
+import 'package:mechanix_clock/l10n/app_localizations.dart';
 
 class MainNavigationContainer extends StatefulWidget {
   const MainNavigationContainer({super.key});
 
   @override
-  State<MainNavigationContainer> createState() => _MainNavigationContainerState();
+  State<MainNavigationContainer> createState() =>
+      _MainNavigationContainerState();
 }
 
 class _MainNavigationContainerState extends State<MainNavigationContainer> {
   int _currentIndex = 0;
 
-  final List<Widget> _screens = [
-    const AlarmListScreen(),
-    const StopwatchScreen(),
-    const Center(child: Text('Timer - Coming Soon')),
-    const Center(child: Text('World Clock - Coming Soon')),
-  ];
-
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+    final List<Widget> screens = [
+      const AlarmListScreen(),
+      const StopwatchScreen(),
+      Center(child: Text(l10n.timer_coming_soon)),
+      Center(child: Text(l10n.world_clock_coming_soon)),
+    ];
+
     return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _screens,
-      ),
+      body: IndexedStack(index: _currentIndex, children: screens),
       bottomNavigationBar: BottomNavBar(
         currentIndex: _currentIndex,
         onTap: (index) {
